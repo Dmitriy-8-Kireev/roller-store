08b4b0c7f9ff99b214fbe7392e9eed7dc38266ce
Dmitriy Kireev <chief_programmer@mail.ru>
2019-02-09 16:52:44 +0300

    change app.js, add: cart-page.js, home-page.js, shop-header.js

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import './app.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import ShopHeader from "../shop-header";
+import { HomePage, CartPage } from "../pages";
+
+import "./app.css";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main role="main" className="container">
+      <ShopHeader numItems={1} total={10000} />
+      <Switch>
+        <Route path="/" component={HomePage} exact />
+
+        <Route path="/cart" component={CartPage} />
+      </Switch>
+    </main>
+  );
 };
 
 export default App;
