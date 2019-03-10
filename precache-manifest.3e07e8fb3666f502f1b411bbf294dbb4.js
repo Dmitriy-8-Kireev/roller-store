89c79388fd6508f74efc567712093b9f7bf8d2d8
Dmitriy Kireev <chief_programmer@mail.ru>
2019-03-10 20:32:11 +0300

    Updates

---
+++ precache-manifest.3e07e8fb3666f502f1b411bbf294dbb4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a6982f8832db2f91cbf",
+    "url": "/roller-store/static/js/runtime~main.0a6982f8.js"
+  },
+  {
+    "revision": "fff878dc6c4073ec74dd",
+    "url": "/roller-store/static/js/main.fff878dc.chunk.js"
+  },
+  {
+    "revision": "5ea52242d5ab7cf38f3d",
+    "url": "/roller-store/static/js/1.5ea52242.chunk.js"
+  },
+  {
+    "revision": "fff878dc6c4073ec74dd",
+    "url": "/roller-store/static/css/main.c269ec3e.chunk.css"
+  },
+  {
+    "revision": "6063207d8442e1f47eb02a22ea73bfc5",
+    "url": "/roller-store/index.html"
+  }
+];
